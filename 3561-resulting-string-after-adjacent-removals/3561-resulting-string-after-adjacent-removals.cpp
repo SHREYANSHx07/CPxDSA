586f550cfd568627306bae617faac2f3cab94b48
SHREYANSHx07 <shreyansh1418@gmail.com>
2025-05-25 14:18:48 +0530

    Time: 81 ms (45.54%), Space: 61.6 MB (41.99%) - LeetHub

--- 3561-resulting-string-after-adjacent-removals/3561-resulting-string-after-adjacent-removals.cpp
+++ 3561-resulting-string-after-adjacent-removals/3561-resulting-string-after-adjacent-removals.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     string resultingString(string s) {
-        string s2=s;
-        while (true) {
-            string s3 = "";
-            int i = 0,x=1;
-            while (i < s2.size()) {
-                if (i < s2.size() - 1) 
-                {
-                    char a = s2[i], b = s2[i + 1];
-                    if (((a == 'a' && b == 'z') || (a == 'z' && b == 'a') || abs(a - b) == 1) && x==1) {
-                        i += 2; 
-                        x=0;
-                        continue;
-                    }
-                }
-                s3 += s2[i];
-                i++;
+        // string s2=s;
+        // while (true) {
+        //     string s3 = "";
+        //     int i = 0,x=1;
+        //     while (i < s2.size()) {
+        //         if (i < s2.size() - 1) 
+        //         {
+        //             char a = s2[i], b = s2[i + 1];
+        //             if (((a == 'a' && b == 'z') || (a == 'z' && b == 'a') || abs(a - b) == 1) && x==1) {
+        //                 i += 2; 
+        //                 x=0;
+        //                 continue;
+        //             }
+        //         }
+        //         s3 += s2[i];
+        //         i++;
+        //     }
+        //     if (s2.size() == s3.size()) break; 
+        //     s2 = s3;
+        // }
+        // return s2;
+        stack<char> st;
+        for(auto i:s)
+        {
+            if(st.empty())
+            {
+                st.push(i);
+                continue;
             }
-            if (s2.size() == s3.size()) break; 
-            s2 = s3;
+            char x=st.top();
+            if(abs(x-i)==1 || abs(x-i)==25) st.pop();
+            else st.push(i);
         }
-        return s2;
+        string ans="";
+        while(st.size())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
