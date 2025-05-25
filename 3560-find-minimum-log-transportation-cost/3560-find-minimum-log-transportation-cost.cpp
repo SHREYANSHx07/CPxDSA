aa08b9be402d0f6f0bd846a32f22c024fc116999
SHREYANSHx07 <shreyansh1418@gmail.com>
2025-05-25 13:37:52 +0530

    Time: 0 ms (100%), Space: 8.8 MB (26.07%) - LeetHub

--- 3560-find-minimum-log-transportation-cost/3560-find-minimum-log-transportation-cost.cpp
+++ 3560-find-minimum-log-transportation-cost/3560-find-minimum-log-transportation-cost.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     long long minCuttingCost(int n, int m, int k) {
+        long long ans;
         if(n<=k && m<=k)
         return 0;
         else if(n<=k && m>k && m<=2*k)
-        return k*(m-k);
+        ans = (long long)k*(long long)((m-k));
         else if(m<=k && n>k && n<=2*k)
-        return k*(n-k);
+        ans =(long long)k*(long long)(n-k);
         else
-        return -1;
+        ans= -1;
+        return ans;
         
     }
 };
