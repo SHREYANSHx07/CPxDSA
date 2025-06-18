00cd5c08321c573c9f1dcf1d4171659559a2b691
SHREYANSHx07 <shreyansh1418@gmail.com>
2025-06-18 09:04:27 +0530

    Time: 34 ms (97.13%), Space: 120 MB (64.59%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -5,15 +5,11 @@ public:
         sort(v.begin(),v.end());
         for(int i=0;i<v.size();i+=3)
         {
-            vector<int> u;
-            u.push_back(v[i]);
-            u.push_back(v[i+1]);
-            u.push_back(v[i+2]);
             if((v[i+1]-v[i])>k || v[i+2]-v[i+1]>k || v[i+2]-v[i]>k)
             {
                 return {};
             }
-            ans.push_back(u);
+            ans.push_back({v[i],v[i+1],v[i+2]});
         }
         return ans;
         
