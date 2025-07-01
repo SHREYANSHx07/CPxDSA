5c059e079d332636011b2838b4cfd84d41307708
SHREYANSHx07 <shreyansh1418@gmail.com>
2025-07-01 14:11:49 +0530

    Time: 0 ms (100%), Space: 9 MB (10.92%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.cpp
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     int possibleStringCount(string s) {
         int ans=1;
-        char x=s[0];
-        int y=0;
         for(int i=1;i<s.size();i++)
         {
-            // if(s[i]==x) y++;
-            // else
-            // {
-            //     ans+=y;
-            //     y=0;
-            //     x=s[i];
-            // }
             if(s[i]==s[i-1]) ans++;
         }
-        //if(y) ans+=y;
         return ans;
         
     }
