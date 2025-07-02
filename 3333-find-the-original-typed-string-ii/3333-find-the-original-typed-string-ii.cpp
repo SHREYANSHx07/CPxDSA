3e88110a0e7692643ca252d94cd16673e87de5c6
SHREYANSHx07 <shreyansh1418@gmail.com>
2025-07-02 17:02:20 +0530

    Time: 357 ms (57.14%), Space: 175.3 MB (25%) - LeetHub

---
+++ 3333-find-the-original-typed-string-ii/3333-find-the-original-typed-string-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int possibleStringCount(string word, int k) {
+        int MOD = 1e9 + 7;
+        vector<int> groups;
+        int count = 1;
+        long total = 1;
+        vector<int> dp(k, 0);
+        dp[0] = 1;
+        long invalid = 0;
+
+        if(word.empty()){
+            return 0;
+        }
+
+        for(int i = 1; i < word.size(); i++){
+            if(word[i] == word[i - 1]){
+                count++;
+            }
+            else{
+                groups.push_back(count);
+                count = 1;
+            }
+        }
+        groups.push_back(count);
+
+        for(int g : groups){
+            total = (total * g) % MOD;
+        }
+
+        if(k <= groups.size()){
+            return total;
+        }
+
+        for(int g : groups){
+            vector<int> new_dp(k, 0);
+            long sum = 0;
+
+            for(int s = 0; s < k; s++){
+                if(s > 0){
+                    sum = (sum + dp[s-1]) % MOD;
+                }
+                if(s > g){
+                    sum = (sum - dp[s-g-1] + MOD) % MOD;
+                }
+                new_dp[s] = sum;
+            }
+            dp = new_dp;
+        }
+
+        for (int s = groups.size(); s < k; s++){
+            invalid = (invalid + dp[s]) % MOD;
+        }
+
+        return (total - invalid + MOD) % MOD;
+    }
+};
